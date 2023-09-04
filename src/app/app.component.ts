c389f8974f873d9ebab1ef58b9edbb7700e32314
BihanMathieu <mathieu.bihan35@gmail.com>
2023-09-04 15:59:46 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'route';
+
+  constructor(iconRegistry:MatIconRegistry, sanitizer:DomSanitizer){
+    iconRegistry.addSvgIcon(
+      'coucou',
+      sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/img/icons/coucou.svg'
+      )
+    )
+  }
 }
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+    <p>
+      page-not-found works!
+      <a routerLink="/home">home</a>
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PageNotFoundComponent {
+
+}
